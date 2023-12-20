cd7b2edf933c3b6f9dda355c79f791e118870f47
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2023-12-20 13:59:41 +0000

    PISA Covid

--- pisa_covid.R
+++ pisa_covid.R
@@ -1,7 +1,7 @@
 library(readxl)
 library(httr)
 library(tidyr)
-
+library(dplyr)
 # URL of the file
 UNESCO_link <- "https://covid19.uis.unesco.org/wp-content/uploads/sites/11/2021/05/UNESCO_school_closures_database.xlsx"
 
@@ -28,3 +28,19 @@ pisa <- pisa %>%
 pisa <- pisa[,-c(306:309)]
 pisa$`Year/Study` <- as.numeric(pisa$`Year/Study`)
 pisa$Average <- as.numeric(pisa$Average)
+
+
+# Assuming 'pisa' is the data frame provided by you
+# Filtering the data for the years 2022 and 2018
+pisa_2022 <- filter(pisa, `Year/Study` == 2022)
+pisa_2018 <- filter(pisa, `Year/Study` == 2018)
+
+# Merging the 2022 and 2018 data on 'Jurisdiction'
+merged_data <- inner_join(pisa_2022, pisa_2018, by = "Jurisdiction", suffix = c("_2022", "_2018"))
+
+# Calculating the difference
+merged_data$difference <- merged_data$Average_2022 - merged_data$Average_2018
+
+# Creating the new data frame with required columns
+pisa_difference <- select(merged_data, country = Jurisdiction, difference)
+pisa_difference
